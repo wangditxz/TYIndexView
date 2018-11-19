8dce99dc043a29d8ccf6a91aaee4da3e94554982
王迪 <wangdi05@yxqiche.com>
2018-11-19 14:44:01 +0800

    建立项目

---
+++ TYIndexView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TYIndexView
+//
+//  Created by 王迪 on 2018/11/19.
+//  Copyright © 2018年 王迪. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
